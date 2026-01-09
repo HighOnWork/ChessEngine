7fd9619ce985e26d228005b3c14d38541c337de6
Muhammad Ahmad <highonworking@gmail.com>
2026-01-09 18:29:46 +0500

    Remove unused code and comments
    Cleaned up chess_board.py and main.py by removing unnecessary whitespace and commented-out code related to piece movement and spawning. This improves code readability and maintainability.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ canvas = tk.Canvas(height=1000, width=1000)
 
 new_chess_board = ChessBoard(windowMaker=windowMaker, canvas=canvas)
 chess_pieces = ChessPieces(canvas=canvas, square_size=125)
-# movement_of_indi = movement_of_indivisual_pieces(canvas=canvas)
 
 def config():
         canvas.grid(column=0, row=0)
@@ -26,9 +25,5 @@ for i in range(8):
     piece_type = back_rank[i]
     chess_pieces.spawn_pieces("b"+piece_type, i , 0)
     chess_pieces.spawn_pieces("w"+piece_type, i, 7)
-# chess_pieces.spawn_pieces("wq" ,3, 7)
-# chess_pieces.spawn_pieces("bq" ,3, 0)
-# chess_pieces.spawn_pieces("wk" ,4, 7)
-# canvas.tag_bind(square, "<Button-1>", lambda event, s=square: self.button_clicked_for_black_rooks(event=event, pawn_item_id=rook_item_id, clicked_square_id=s))
 
 config()
